cea3ba06dd9eb834006337aac18f7ac411bc8252
Maziya Ats Tsaqofi <122241381+maziyaqofi@users.noreply.github.com>
2023-11-17 18:29:08 +0700

    Add files via upload

---
+++ simple_project.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //simple project
+    //maziya ats tsaqofi
+    //17 november 2023
+
+    cout << "Nama Saya Maziya Ats Tsaqofi" <<endl; //menampilkan kalimat dengan cout
+    cout << "Saya suka belajar "; // tidak ada endl makan kalimat akan nyambung
+    cout << "computer sience"; //akan nyambung dengan kalimat sebelumnya
+
+    return 0;
+}
