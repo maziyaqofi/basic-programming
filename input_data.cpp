90fbed61e6dfb954fd55d7c546f507719c22a1c1
Maziya Ats Tsaqofi <122241381+maziyaqofi@users.noreply.github.com>
2023-11-18 05:03:18 +0700

    Add files via upload

---
+++ input_data.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // input data
+    // Maziya Ats Tsaqofi
+    // 17 November 2023
+
+    string nama;
+    string hobi;
+    string negara;
+
+    // cout di bawah ini hanya bisa memasukan satu kata
+
+    cout << "nama saya: "; //tampilan yang muncul
+    cin >> nama; // tempat menampung isinya
+
+    cout << "hobi: ";
+    cin >> hobi;
+
+    cout << "negara yang di tinggali: ";
+    cin >> negara;
+
+    return 0;
+}
